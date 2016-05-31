68f79dd8f78fa1b25ed9427188fd0dcd92ff221b
pengyanan <PengYanan@192.168.10.11>
2016-05-31 11:25:21 +0800

    增加下载验证码图片功能

--- src/main/java/com/pyn/test/JD.java
+++ src/main/java/com/pyn/test/JD.java
@@ -1,32 +1,41 @@
 package com.pyn.test;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.client.HttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.message.BufferedHeader;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JD {
   // The configuration items
 
+  private Logger logger = LoggerFactory.getLogger(JD.class);
 
-  private static String redirectURL = "http://order.jd.com/center/list.action";
-//  private static String redirectURL = "http://cart.jd.com/cart.action";
+  // private static String redirectURL = "http://order.jd.com/center/list.action";
+  private static String redirectURL = "http://cart.jd.com/cart.action";
   private static String loginUrl = "http://passport.jd.com/uc/login";
 
   // Don't change the following URL
@@ -38,14 +47,31 @@ public class JD {
   private String loginname = "13438389109";
   private String loginpwd = "Pyn19881128";
 
+  // private String loginname = "mapzchen";
+  // private String loginpwd = "9Eit8Dh54A1";
 
+
+
+  /**
+   * 
+   * <p>
+   * Description: 获取Form表单中的nput数据
+   * </p>
+   * 
+   * @return
+   * @author Peng Yanan
+   * @date 2016年5月31日
+   */
   public Map<String, String> getParams() {
     Map<String, String> map = new HashMap<String, String>();
     String html = getText(loginUrl);
+    // System.out.println(html);
     Document doc = Jsoup.parse(html);
+    // 获取uuid
     Element uuidElement = doc.getElementById("uuid");
     String uuid = uuidElement.attr("value");
     map.put("uuid", uuid);
+
     Element content = doc.getElementById("formlogin");
     Elements inputs = content.getElementsByTag("input");
     for (Element element : inputs) {
@@ -59,6 +85,56 @@ public class JD {
     return map;
   }
 
+  /**
+   * 
+   * <p>
+   * Description: 下载验证码图片
+   * </p>
+   * 
+   * @param doc
+   * @author Peng Yanan
+   * @date 2016年5月31日
+   */
+  public void authcode(Document doc) {
+    Element imgElement = doc.getElementById("JD_Verification1");
+    String url = "http:" + imgElement.attr("src2");
+    System.out.println(url);
+    HttpClient httpclient2 = new DefaultHttpClient();
+    HttpPost httpost2 = new HttpPost(url);
+    HttpResponse response2;
+    try {
+      response2 = httpclient2.execute(httpost2);
+      HttpEntity imgEntity = response2.getEntity();
+      if (imgEntity != null) {
+        byte[] bit = EntityUtils.toByteArray(imgEntity);
+        BufferedOutputStream out = null;
+        if (bit.length > 0) {
+          try {
+            out = new BufferedOutputStream(new FileOutputStream("/authImg/authcode.jpg"));
+            logger.info("authcode.jpg download successfully");
+            out.write(bit);
+            out.flush();
+          } finally {
+            if (out != null)
+              out.close();
+          }
+        }
+      }
+    } catch (ClientProtocolException e) {
+      logger.error("error message", e);
+      e.printStackTrace();
+    } catch (IOException e) {
+      logger.error("error message", e);
+    }
+  }
+
+  /**
+   * 
+   * <p>Description: 登陆</p>
+   * @return
+   * @author Peng Yanan
+   * @date      2016年5月31日
+   */
   private boolean login() {
     Map<String, String> map = getParams();
 
@@ -66,9 +142,6 @@ public class JD {
     // All the parameters post to the web site
     List<BasicNameValuePair> nvps = new ArrayList<BasicNameValuePair>();
     nvps.add(new BasicNameValuePair("ReturnUrl", redirectURL));
-    // nvps.add(new BasicNameValuePair("loginname", Constant.userName));
-    // nvps.add(new BasicNameValuePair("nloginpwd", Constant.password));
-    // nvps.add(new BasicNameValuePair("loginpwd", Constant.password));
     nvps.add(new BasicNameValuePair("loginname", loginname));
     nvps.add(new BasicNameValuePair("nloginpwd", loginpwd));
     nvps.add(new BasicNameValuePair("loginpwd", loginpwd));
@@ -97,6 +170,14 @@ public class JD {
     return true;
   }
 
+  
+  /**
+   * 
+   * <p>Description: 获取location信息</p>
+   * @return
+   * @author Peng Yanan
+   * @date      2016年5月31日
+   */
   private String getRedirectLocation() {
     BufferedHeader locationHeader = (BufferedHeader) response.getFirstHeader("Location");
     if (locationHeader == null) {
@@ -105,6 +186,14 @@ public class JD {
     return locationHeader.getValue();
   }
 
+  /**
+   * 
+   * <p>Description: 抓取数据</p>
+   * @param redirectLocation
+   * @return
+   * @author Peng Yanan
+   * @date      2016年5月31日
+   */
   private String getText(String redirectLocation) {
     HttpGet httpget = new HttpGet(redirectLocation);
     ResponseHandler<String> responseHandler = new BasicResponseHandler();
@@ -116,11 +205,16 @@ public class JD {
       responseBody = null;
     } finally {
       httpget.abort();
-      // httpclient.getConnectionManager().shutdown();
     }
     return responseBody;
   }
 
+  /**
+   * 
+   * <p>Description: 登陆成功后 打印跳转页面的内容</p>
+   * @author Peng Yanan
+   * @date      2016年5月31日
+   */
   public void printText() {
     if (login()) {
       System.out.println(getText(redirectURL));
@@ -133,7 +227,6 @@ public class JD {
 
   public static void main(String[] args) {
     JD renRen = new JD();
-    // renRen.getParams();
     renRen.printText();
   }
 }
